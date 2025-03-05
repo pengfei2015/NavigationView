048724cc490fc634e83594964b33d65a28b52d41
feiliu <feiliu40@163.com>
2025-03-05 15:18:44 +0800

    侧滑失效了,去掉手势拦截范围

--- Sources/Internal/Views/NavigationView.swift
+++ Sources/Internal/Views/NavigationView.swift
@@ -25,7 +25,7 @@ struct NavigationView: View {
         ZStack { ForEach(temporaryViews, id: \.id, content: createItem) }
             .ignoresSafeArea()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .gesture(createDragGesture(), including: .subviews)
+            .gesture(createDragGesture())
             .onChange(of: stack.views, perform: onViewsChanged)
             .onChange(of: isGestureActive, perform: onDragGestureEnded)
             .onAnimationCompleted(for: animatableData.opacity, perform: onAnimationCompleted)
